5083b5e52b63d21b9d76947006537462552607c8
hunterkorgel <hunterk13@gmail.com>
2020-08-20 18:14:47 -0700

    Added Unix executable packaging

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import io
+import os
+import sys
 import random
 
 browser = webdriver.Chrome(ChromeDriverManager().install())  # driver
@@ -33,13 +35,13 @@ def tweet_scroller(url):
 def txt_writer(target_html):
     file_out = 'webreturn.txt'
 
-    with io.open(file_out, 'w', encoding='utf-8') as txtfile:
+    with io.open(os.path.join(os.path.dirname(sys.executable), file_out), 'w', encoding='utf-8') as txtfile:
         txtfile.write(target_html)
 
 # Function to read urls txt file and randomly select a url for handling
 def url_selector():
 
-    url_collection = open('URLs.txt', 'r')
+    url_collection = open(os.path.join(os.path.dirname(sys.executable), 'URLs.txt'), 'r')
     lines = url_collection.readlines()
 
     return random.choice(lines)
