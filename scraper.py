c492cb39a3f6417184414880984f157d860552de
hunterkorgel <hunterk13@gmail.com>
2020-09-10 20:15:00 -0700

    first working updates to be repackaged

--- scraper.py
+++ scraper.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import importlib
 import time
 import io
 import os
 import sys
 import random
 
-browser = webdriver.Chrome(ChromeDriverManager().install())  # driver
+f = io.open(os.path.join(os.path.dirname(sys.executable), file_out), 'w', encoding='utf-8')
+global config
+config = importlib.load_source('data', '', f)
+f.close()
+
+if config.headless:
+    options = Options()
+    options.headless = True
+
+    browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # driver
+
+else:
+    browser = webdriver.Chrome(ChromeDriverManager().install()) # driver
 
 # This function handles dynamic page content loading using Selenium
 def tweet_scroller(url):
@@ -14,7 +28,9 @@ def tweet_scroller(url):
 
     last_height = browser.execute_script('return document.body.scrollHeight')
 
-    while True:
+    page_count = config.pages
+
+    while page_count > 0:
         browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
         # Define how many seconds to wait while dynamic page content loads
@@ -25,6 +41,8 @@ def tweet_scroller(url):
             break
         else:
             last_height = new_height
+
+        page_count -= 1
 
     page_html = browser.page_source
 
@@ -45,6 +63,12 @@ def url_selector():
     lines = url_collection.readlines()
 
     return random.choice(lines)
+
+# Function to read config txt file and return scroll time settings
+def config_reader():
+
+    configuration = yaml.safe_load(open(os.path.join(os.path.dirname(sys.executable), 'config.txt'), 'r'))
+
 
     # main
 if __name__ == "__main__":
